d76ea6946fd531a600fa762a98ef796e13fc464d
Silviu Iordache <silviu.iordache92@gmail.com>
2021-12-05 23:09:51 +0200

    Better code structure

---
+++ src/ColorPicker/ColorPicker.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: [
+        'red',
+        'green',
+        'yellow'
+      ]
+    };
+   
+  }
+
+  render() {
+    return (
+      <div>ceva</div>
+    )
+  }
+}
--- src/Toolbar/Toolbar.js
+++ src/Toolbar/Toolbar.js
@@ -1,39 +1,41 @@
 import React from "react";
-import BrushSizeSelector from './BrushSizeSelector';
+import BrushSizeSelector from "../BrushSizeSelector/BrushSizeSelector.js";
+import ColorPicker from "../ColorPicker/ColorPicker.js";
+
 import "./Toolbar.css";
 
 export default class Toolbar extends React.Component {
-
   selectBrushSize = (size) => {
-    this.props.selectBrushSize(size)
-  }
+    this.props.selectBrushSize(size);
+  };
 
   eraseCanvas = (props) => {
     this.props.eraseCanvas();
-  }
+  };
 
   render() {
     const style = {
-      width: this.props.toolbarWidth
-    }
+      width: this.props.toolbarWidth,
+    };
     return (
-      <div className='toolbar-container d-flex justify-content-end align-items-center' style={style}>
-        <BrushSizeSelector onBrushSizeSelected={this.selectBrushSize}/>
+      <div
+        className="toolbar-container d-flex justify-content-end align-items-center"
+        style={style}
+      >
+        <ColorPicker />
+        <BrushSizeSelector onBrushSizeSelected={this.selectBrushSize} />
         <div className="ms-3 h-100">
-          <EraseCanvasButton eraseCanvas={this.eraseCanvas}/>
+          <EraseCanvasButton eraseCanvas={this.eraseCanvas} />
         </div>
       </div>
-    )
+    );
   }
 }
 
 function EraseCanvasButton(props) {
   return (
-    <button 
-      className='btn btn-danger px-4 h-100' 
-      onClick={props.eraseCanvas}
-    >
+    <button className="btn btn-danger px-4 h-100" onClick={props.eraseCanvas}>
       <i className="far fa-trash-alt fa-2x"></i>
     </button>
-  )
+  );
 }
