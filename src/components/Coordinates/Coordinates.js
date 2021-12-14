6c7f367fc9692c8952240f5996e831ee81033d0f
Silviu Iordache <silviu.iordache92@gmail.com>
2021-12-14 22:42:17 +0200

    Update coordinates

--- src/components/Coordinates/Coordinates.js
+++ src/components/Coordinates/Coordinates.js
@@ -1,8 +1,10 @@
 export default function Coordinates(props) {
   return (
-    <div id="coordinates">
-      <p> x: {props.x} </p>
-      <p> y: {props.y} </p>
+    <div id="coordinates"> 
+      <p> cursorX: {props.cursorX} </p>
+      <p> cursorY: {props.cursorY} </p>
+      <p> canvasX: {props.canvasX} </p>
+      <p> canvasY: {props.canvasY} </p>
     </div>
   )
 }
