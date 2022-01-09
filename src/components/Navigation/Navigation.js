0263b53bdb76fbc931baf59692122d4b608ee429
Silviu Iordache <silviu.iordache92@gmail.com>
2022-01-09 13:59:52 +0200

    Change navbar home link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,8 +4,8 @@ export default function Navigation() {
   return (
     <div className="row mb-2">
       <div className="col-12 bg-light py-4 d-flex justify-content-center">
-        <Link to="/" className="me-5">Draw</Link>
-        <Link to="gallery">Gallery</Link>
+        <Link to="/" className="me-5">Home</Link>
+        <Link to="/gallery">Gallery</Link>
       </div>
     </div>
   )
