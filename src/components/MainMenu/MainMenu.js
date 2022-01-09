dda89d004e6962a4180c736595e1615f33d68ce0
Silviu Iordache <silviu.iordache92@gmail.com>
2022-01-09 14:27:04 +0200

    Refactor to function

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -4,25 +4,16 @@ import {
   
 } from "react-router-dom";
 
-export default class MainMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ceva: false,
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="mb-5 mt-5">
-          <h1>Welcome to Painty 🎨</h1>
-          <p>Painty is a drawing game in which you draw agaisnt the clock</p>
-        </div>
-          <Link to="/draw/challenge" className="me-5">Start ⏳</Link>
-          <Link to="/draw/practice" className="me-5">Practice 🖌</Link>
-          <Link to="/gallery">Gallery 🖼</Link>
+export default function MainMenu() {
+  return (
+    <div>
+      <div className="mb-5 mt-5">
+        <h1>Welcome to Painty 🎨</h1>
+        <p>Painty is a drawing game in which you draw agaisnt the clock</p>
       </div>
-    )
-  }
+        <Link to="/draw/challenge" className="me-5">Start ⏳</Link>
+        <Link to="/draw/practice" className="me-5">Practice 🖌</Link>
+        <Link to="/gallery">Gallery 🖼</Link>
+    </div>
+  )
 }
