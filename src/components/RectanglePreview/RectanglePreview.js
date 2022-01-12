2cff953965fd07e5d4ac838ec63fbe281c6eb417
Silviu Iordache <silviu.iordache92@gmail.com>
2022-01-12 11:25:17 +0200

    Simplify

--- src/components/RectanglePreview/RectanglePreview.js
+++ src/components/RectanglePreview/RectanglePreview.js
@@ -1,29 +1,12 @@
-import React from "react";
+export default function RectanglePreview() {
+  const previewRectStyle = {
+    width: `${this.props.width}px`,
+    height: `${this.props.height}px`,
+    position: "absolute",
+    backgroundColor: "pink",
+    top: `${this.props.y}px`,
+    left: `${this.props.x}px`,
+  };
 
-export default class RectanglePreview extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
-  render() {
-    const previewRectStyle = {
-      width: `${this.props.width}px`,
-      height: `${this.props.height}px`,
-      position: 'absolute',
-      backgroundColor: 'pink',
-      top: `${this.props.y}px`,
-      left: `${this.props.x}px`
-    }
-
-    return (
-      <div
-        style={previewRectStyle}
-        width="30px"
-        height="30px"
-      />
-    )
-  }
+  return <div style={previewRectStyle} width="30px" height="30px" />;
 }
