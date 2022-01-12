302119279b7aeeda16c283bf059031543c8744c1
Silviu Iordache <silviu.iordache92@gmail.com>
2022-01-12 11:24:23 +0200

    Simplify ToolSelector

--- src/components/ToolSelector/ToolSelector.js
+++ src/components/ToolSelector/ToolSelector.js
@@ -1,60 +1,50 @@
 import React from "react";
-export default class Toolbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tools: [
-        {
-          name: 'Brush Tool',
-          icon: 'fas fa-paint-brush'
-        },
-        {
-          name: 'Eraser Tool',
-          icon: 'fas fa-eraser'
-        },
-        {
-          name: 'Paint Bucket Tool',
-          icon: 'fas fa-fill-drip'
-        }
-      ]
-    }
-  }
-  render() {
-    const tools = this.state.tools.map((tool, index) => {
-      return (
-        <ToolButton
-          active={this.props.currentTool === tool.name}
-          icon={tool.icon}
-          name={tool.name}
-          key={index}
-          changeTool={this.props.changeTool}
-        />
-      )
-    })
+export default function Toolbar(props) {
+  const tools = [
+    {
+      name: "Brush Tool",
+      icon: "fas fa-paint-brush",
+    },
+    {
+      name: "Eraser Tool",
+      icon: "fas fa-eraser",
+    },
+    {
+      name: "Paint Bucket Tool",
+      icon: "fas fa-fill-drip",
+    },
+  ];
+
+  const toolButtons = tools.map((tool, index) => {
     return (
-      <div>
-        {tools}
-      </div>
-    )
-  }
+      <ToolButton
+        active={props.currentTool === tool.name}
+        icon={tool.icon}
+        name={tool.name}
+        key={index}
+        changeTool={props.changeTool}
+      />
+    );
+  });
+  return <div> {toolButtons} </div>;
 }
 
 function ToolButton(props) {
   let activeStyle;
   if (props.active) {
     activeStyle = {
-      border: '2px solid black',
-      color: 'black',
-      backgroundColor: '#ebab34'
-    } 
+      border: "2px solid black",
+      color: "black",
+      backgroundColor: "#ebab34",
+    };
   }
   return (
-    <button 
+    <button
       className="btn btn-outline-secondary"
       style={activeStyle}
       onClick={() => props.changeTool(props.name)}
     >
       <i className={props.icon}></i>
     </button>
-  )
+  );
 }
