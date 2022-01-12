1033024d4dfe2bd4ab9790f098f282a1ca45747d
Silviu Iordache <silviu.iordache92@gmail.com>
2022-01-12 11:34:09 +0200

    Refactor Gallery to function with useState

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,67 +1,46 @@
-import React from "react";
-import GalleryDrawing from "../GalleryDrawing/GalleryDrawing.js"
+import React, { useState } from "react";
+import GalleryDrawing from "../GalleryDrawing/GalleryDrawing.js";
 
-export default class DrawingBoard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      storageImages: [],
-    }
-  }
+export default function DrawingBoard() {
+  const [images, setImages] = useState(
+    JSON.parse(localStorage.getItem("paintyImages"))
+  );
 
-  componentDidMount() {
-    this.getDrawingsFromStorage();
-  }
-
-  getDrawingsFromStorage = () => {
-    const storageImages = JSON.parse(localStorage.getItem('paintyImages'));
-    this.setState({
-      storageImages
-    })
-  }
-
-  deleteImage = (id) => {
+  function deleteImage(id) {
     // eslint-disable-next-line no-restricted-globals
-    const confirmDelete = confirm('Are you sure you want to delete this image?');
+    const confirmDelete = confirm(
+      "Are you sure you want to delete this image?"
+    );
 
-    if (!confirmDelete)
-      return
-    const galleryImages = JSON.parse(localStorage.getItem("paintyImages"));
+    if (!confirmDelete) return;
+    const updatedImages = JSON.parse(localStorage.getItem("paintyImages"));
 
     // find index of id
-    const indexToDelete = galleryImages.findIndex((elem) => elem.id === id);
-    
+    const indexToDelete = updatedImages.findIndex((elem) => elem.id === id);
+
     // delete elem at that index
-    galleryImages.splice(indexToDelete, 1);
+    updatedImages.splice(indexToDelete, 1);
 
     // update storage
-    localStorage.setItem("paintyImages", JSON.stringify(galleryImages));
+    localStorage.setItem("paintyImages", JSON.stringify(updatedImages));
 
-    this.setState({
-      storageImages: galleryImages
-    })
+    setImages(updatedImages);
   }
 
-  render() {
-    let galleryImages;
-    if (this.state.storageImages) {
-      galleryImages = this.state.storageImages.map((image, index) =>
-        <GalleryDrawing
-          src={image.src}
-          alt={image.name}
-          name={image.name}
-          id={image.id}
-          key={index}
-          deleteImage={this.deleteImage}
-        />
-      );
-    } else {
-      galleryImages = <p className="text-light">Your saved drawings will appear here</p>
-    }
-    return (
-      <div className="row bg-secondary p-5">
-        {galleryImages}
-      </div>
-    )
+  let imageElements = (
+    <p className="text-light">Your saved drawings will appear here</p>
+  );
+  if (images) {
+    imageElements = images.map((image, index) => (
+      <GalleryDrawing
+        src={image.src}
+        alt={image.name}
+        name={image.name}
+        id={image.id}
+        key={index}
+        deleteImage={deleteImage}
+      />
+    ));
   }
+  return <div className="row bg-secondary p-5">{imageElements}</div>;
 }
