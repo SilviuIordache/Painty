40ee6ab8a2074f807814fcea39a618e8ed50f64d
Silviu Iordache <silviu.iordache92@gmail.com>
2022-01-14 01:34:26 +0200

    Giant refactor

--- src/components/DrawingBoard/Canvas.js
+++ src/components/DrawingBoard/Canvas.js
@@ -1,218 +1,181 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import FloodFill from 'q-floodfill';
 import BrushCursor from "../BrushCursor/BrushCursor.js";
-
+import useEventListener from "../../hooks/useEventListener";
 
 import "./DrawingBoard.css";
 
-export default class Canvas extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cursorX: 0,
-      cursorY: 0,
-      canvasAbsoluteX: 0,
-      canvasAbsoluteY: 0,
-      canvasRelativeX: 0,
-      canvasRelativeY: 0,
-      canvasAbsoluteWidth: 800,
-      canvasAbsoluteHeight: 600,
-      canvasRelativeWidth: 0,
-      canvasRelativeHeight: 0,
-      ctx: null,
-      mousePressed: false,
-      canvasHovered: true,
-    };
-  }
+export default function Canvas (props) {
 
-  componentDidMount() {
-    this.setupCanvas();
+  useEventListener("mousemove", handleMouseMove);
+  useEventListener("mousedown", handleMouseDown);
+  useEventListener("mouseup", handleMouseUp);
+  useEventListener("resize", handleWindowResize);
 
-    document.addEventListener("mousemove", this.handleMouseMove);
-    document.addEventListener("mousedown", this.handleMouseDown);
-    document.addEventListener("mouseup", this.handleMouseUp);
-    window.addEventListener('resize', this.handleWindowResize);
-  }
+  const canvasRef = useRef();
+  const [ctx, setCtx] = useState(0);
 
-  componentWillUnmount() {
-    document.removeEventListener("mousemove", this.handleMouseMove);
-    document.removeEventListener("mousedown", this.handleMouseDown);
-    document.removeEventListener("mouseup", this.handleMouseUp);
-    window.removeEventListener('resize', this.handleWindowResize);
-  }
+  // initialize canvas
+  useEffect(() => {
+    setupCanvas();
+  }, [])
 
-  setupCanvas = () => {
-    const canvas = document.getElementById("canvas");
+  function setupCanvas () {
+    const canvas = canvasRef.current;
+
     canvas.style.backgroundColor = "white";
-    canvas.width = this.state.canvasAbsoluteWidth;
-    canvas.height = this.state.canvasAbsoluteHeight;
-
+    canvas.width = canvasAbsoluteWidth;
+    canvas.height = canvasAbsoluteHeight;
     canvas.style.width ='90%';
     canvas.style.height='auto';
 
-    this.setState(
-      { ctx: canvas.getContext("2d")},
-      () => {
-        this.applyWhiteBackground();
-      }
-    );
-    this.calculcateCanvasRelativeSize();
-  };
+    const newCTX = canvas.getContext("2d");
+
+    newCTX.fillStyle = "#ffffff";
+    newCTX.fillRect(0, 0, canvasAbsoluteWidth, canvasAbsoluteHeight);
+
+    setCtx(newCTX);
+    calculcateCanvasRelativeSize();
+  }
 
 
-  applyWhiteBackground = () => {
-    this.drawRectangle(
-      0,
-      0,
-      this.state.canvasAbsoluteWidth,
-      this.state.canvasAbsoluteHeight,
-      "#ffffff"
-    );
-  };
+  const [mousePressed, setMousePressed] = useState(false);
+  let [canvasHovered, setCanvasHovered] = useState(true);
 
-  handleMouseMove = (e) => {
-    const canvas = document.getElementById("canvas");
-    const rect = canvas.getBoundingClientRect();
+  const [cursorX, setCursorX] = useState(0);
+  const [cursorY, setCursorY] = useState(0);
 
-    const cursorX = e.clientX + window.pageXOffset;
-    const cursorY = e.clientY + window.pageYOffset;
+  const [canvasRelativeX, setCanvasRelativeX] = useState(0);
+  const [canvasRelativeY, setCanvasRelativeY] = useState(0);
+
+  const [canvasAbsoluteWidth] = useState(800);
+  const [canvasAbsoluteHeight] = useState(600);
+
+  const [canvasRelativeWidth, setCanvasRelativeWidth] = useState(0);
+  const [canvasRelativeHeight, setCanvasRelativeHeight] = useState(0);
+
+  function handleMouseMove (e) {
+    const rect = canvasRef.current.getBoundingClientRect();
+
+    setCursorX(e.clientX + window.pageXOffset);
+    setCursorY(e.clientY + window.pageYOffset);
+    
+    // THESE CONST CAN BE DIRECTLY CALLED IN THE UPDATES BELOW?
     const canvasAbsoluteX = e.clientX - rect.left;
     const canvasAbsoluteY =  e.clientY - rect.top;
-    const canvasRelativeX = (canvasAbsoluteX * this.state.canvasAbsoluteWidth)/ this.state.canvasRelativeWidth;
-    const canvasRelativeY = (canvasAbsoluteY * this.state.canvasAbsoluteHeight)/ this.state.canvasRelativeHeight;
+    const canvasRelativeX = (canvasAbsoluteX * canvasAbsoluteWidth)/ canvasRelativeWidth;
+    const canvasRelativeY = (canvasAbsoluteY * canvasAbsoluteHeight)/ canvasRelativeHeight;
 
-    this.setState({
-      cursorX,
-      cursorY,
-      canvasAbsoluteX,
-      canvasAbsoluteY,
-      canvasRelativeX,
-      canvasRelativeY
-    });
+    setCanvasRelativeX(canvasRelativeX);
+    setCanvasRelativeY(canvasRelativeY)
 
     // line below draws
-    if (this.state.mousePressed) {
-      if(this.props.currentTool) {
-          this.drawPath();
+    if (mousePressed) {
+      if(props.currentTool) {
+        drawPath();
       }
     }
   };
 
-  floodFill = () => {
-    console.log('aaa')
+  function floodFill () {
     // get image data
-    const imgData = this.state.ctx.getImageData(
+    const imgData = ctx.getImageData(
       0, 
       0, 
-      this.state.canvasAbsoluteWidth,
-      this.state.canvasAbsoluteHeight)
+      canvasAbsoluteWidth,
+      canvasAbsoluteHeight)
 
     // Construct flood fill instance
     const floodFill = new FloodFill(imgData)
 
     // Modify image data
     floodFill.fill(
-      this.props.currentBrushColor,
-      Math.round(this.state.canvasRelativeX),
-      Math.round(this.state.canvasRelativeY),
+      props.currentBrushColor,
+      Math.round(canvasRelativeX),
+      Math.round(canvasRelativeY),
       100
-    )
+    );
 
-    // put the modified data back in context ?
-    this.state.ctx.putImageData(floodFill.imageData, 0, 0)
+    // put the modified data back in context
+    ctx.putImageData(floodFill.imageData, 0, 0)
   }
 
-  handleMouseDown = () => {
-    this.setState({ mousePressed: true });
+  function handleMouseDown () {
+    setMousePressed(true);
 
-    if (this.props.currentTool === "Paint Bucket Tool") {
-      if (this.state.canvasHovered) {
-        this.floodFill();
+    if (props.currentTool === "Paint Bucket Tool") {
+      if (canvasHovered) {
+        floodFill();
       }
     } else {
       // this line allows for single dots by 1-click
-      this.drawPath();
+      drawPath();
     }
   }
 
-  handleMouseUp = () => {
-    this.setState({ mousePressed: false });
-    this.state.ctx.beginPath();
+  function handleMouseUp () {
+    setMousePressed(false);
+    ctx.beginPath();
   }
 
-  drawPath = (e) => {
-    const ctx = this.state.ctx;
-    ctx.lineWidth = this.props.currentBrushSize;
+  function drawPath (e) {
+    ctx.lineWidth = props.currentBrushSize;
     ctx.lineCap = "round";
-    ctx.strokeStyle = this.props.currentBrushColor;
+    ctx.strokeStyle = props.currentBrushColor;
 
-    ctx.lineTo(this.state.canvasRelativeX, this.state.canvasRelativeY);
+    ctx.lineTo(canvasRelativeX, canvasRelativeY);
     ctx.stroke();
-    ctx.moveTo(this.state.canvasRelativeX, this.state.canvasRelativeY);
+    ctx.moveTo(canvasRelativeX, canvasRelativeY);
   };
 
-  handleMouseEnterCanvas = () => {
-    this.setState({ canvasHovered: true },
-      () => {
-        if (this.state.mousePressed) {
-          this.handleMouseDown();
-        }
-      }
-    );
+  function handleMouseEnterCanvas () {
+    setCanvasHovered(true);
+    if (mousePressed) {
+      handleMouseDown();
+    }
   };
 
-  handleMouseLeaveCanvas = () => {
-    this.setState({ canvasHovered: false });
+  function handleMouseLeaveCanvas() {
+    setCanvasHovered(false);
   };
 
-  handleWindowResize = () => {
-    this.calculcateCanvasRelativeSize();
+  function handleWindowResize () {
+    calculcateCanvasRelativeSize();
   }
 
-  calculcateCanvasRelativeSize = () => {
-    const canvas = document.getElementById('canvas').getBoundingClientRect()
-
-    this.setState({
-      canvasRelativeWidth: canvas.width,
-      canvasRelativeHeight: canvas.height
-    })
+  function calculcateCanvasRelativeSize () {
+    const canvasRect = canvasRef.current.getBoundingClientRect();
+    setCanvasRelativeWidth(canvasRect.width);
+    setCanvasRelativeHeight(canvasRect.height);
   }
   
-  drawRectangle = (x, y, width, height, color) => {
-    const ctx = this.state.ctx;
+  function drawRectangle (x, y, width, height, color, ctx) {
     ctx.fillStyle = color || "black";
-
     ctx.fillRect(x, y, width, height);
-
-    this.setState({
-      ctx,
-    });
   };
 
-  render() {
-    return (
-        <div>
-          <div className="row canvas-bg">
-            <div className="col-12 position-relative">
-              <canvas
-                id="canvas"
-                className="no-cursor my-3"
-                onMouseEnter={this.handleMouseEnterCanvas}
-                onMouseLeave={this.handleMouseLeaveCanvas}
-              />
-            </div>
-          </div>
-
-          <BrushCursor
-            hideBrush={!this.state.canvasHovered}
-            size={this.props.currentBrushSize}
-            color={this.props.currentBrushColor}
-            currentTool={this.props.currentTool}
-            x={this.state.cursorX}
-            y={this.state.cursorY}
+  return (
+    <div>
+      <div className="row canvas-bg">
+        <div className="col-12 position-relative">
+          <canvas
+            id="canvas"
+            ref={canvasRef}
+            className="no-cursor my-3"
+            onMouseEnter={handleMouseEnterCanvas}
+            onMouseLeave={handleMouseLeaveCanvas}
           />
         </div>
-        
-    );
-  }
+      </div>
+
+      <BrushCursor
+        hideBrush={!canvasHovered}
+        size={props.currentBrushSize}
+        color={props.currentBrushColor}
+        currentTool={props.currentTool}
+        x={cursorX}
+        y={cursorY}
+      />
+    </div>
+  );
 }
