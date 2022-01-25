283d48cf3cbfb2ec1c8353d49b3f80d9597b920b
Silviu Iordache <silviu.iordache92@gmail.com>
2022-01-25 21:46:24 +0200

    Add back gallery

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <div className="row mb-2">
-      <div className="col-12 bg-light py-4 d-flex justify-content-around">
+    <div className="row mb-2 bg-light rounded">
+      <div className="col-12 py-4 d-flex justify-content-around">
         <Link to="/" className="me-5">Home</Link>
-        {/* <Link to="/gallery">Gallery</Link> */}
+        <Link to="/gallery">Gallery</Link>
       </div>
     </div>
   )
