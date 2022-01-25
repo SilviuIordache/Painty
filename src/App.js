a69bec02c82e22434d5d23d065438079fa1e1e3a
Silviu Iordache <silviu.iordache92@gmail.com>
2022-01-25 20:44:17 +0200

    Add theming

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import DrawingBoard from "./components/DrawingBoard/DrawingBoard";
 import Gallery from "./components/Gallery/Gallery";
 import DrawingDetails from "./components/DrawingDetails/DrawingDetails";
 import "./App.css";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+let theme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+});
 function App() {
   return (
     <div className="App">
       <div className="container">
+        
         <BrowserRouter>
-          <Navigation />
-          <Routes>
-            <Route exact path="/" element={<MainMenu />} />
-            <Route path="/draw/:mode" element={<DrawingBoard />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/drawing/:id" element={<DrawingDetails/>} />
-          </Routes>
+          <ThemeProvider theme={theme}>
+            <Navigation />
+            <Routes>
+              <Route exact path="/" element={<MainMenu />} />
+              <Route path="/draw/:mode" element={<DrawingBoard />} />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route path="/drawing/:id" element={<DrawingDetails/>} />
+            </Routes>
+          </ThemeProvider>
         </BrowserRouter>
       </div>
     </div>
