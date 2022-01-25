122da59e3aec4c4004d06452fc2a019394157a80
Silviu Iordache <silviu.iordache92@gmail.com>
2022-01-25 23:50:16 +0200

    Slightly reduce navbar padding

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navigation() {
   return (
     <div className="row mb-2 bg-light rounded">
-      <div className="col-12 py-4 d-flex justify-content-around">
+      <div className="col-12 py-3 d-flex justify-content-around">
         <Link to="/" className="me-5">Home</Link>
         <Link to="/gallery">Gallery</Link>
       </div>
