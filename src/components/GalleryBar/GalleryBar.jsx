5270346c104b62461bd13421154dcd8b16239a12
Silviu Iordache <silviu.iordache92@gmail.com>
2022-02-02 14:04:34 +0200

    Fix bugs when no drawings

--- src/components/GalleryBar/GalleryBar.jsx
+++ src/components/GalleryBar/GalleryBar.jsx
@@ -4,16 +4,30 @@ export default function GalleryBar(props) {
   return (
     <div className="bg-white rounded py-4">
       <div className="d-flex justify-content-around">
-        <div>Drawings: {props.images.length}</div>
+        <DrawingsTotal images={props.images}/>
         <StorageCapacity images={props.images} />
       </div>
     </div>
   );
 }
 
+function DrawingsTotal(props) {
+
+  let drawings = props?.images?.length || 0;
+  return (
+    <div>Drawings: {drawings}</div>
+  )
+}
+
 function StorageCapacity(props) {
+  
+  let images = []
+  if (props.images) {
+    images = props.images
+  }
+
   let usedStorage = 0;
-  props.images.forEach((image) => {
+  images.forEach((image) => {
     usedStorage = usedStorage + getImageURLSize(image.src);
   });
 
