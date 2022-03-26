9c9c530fafa58163e6a9743cf942d9585436e0a5
Silviu Iordache <silviu.iordache92@gmail.com>
2022-03-26 19:17:15 +0200

    Remove theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import "./App.css";
 
 import Navigation from "./components/Navigation/Navigation";
@@ -8,26 +7,18 @@ import DrawingBoard from "./views/DrawingBoard";
 import Gallery from "./views/Gallery";
 import DrawingDetails from "./views/DrawingDetails";
 
-let theme = createTheme({
-  palette: {
-    mode: 'light'
-  }
-});
 function App() {
   return (
     <div className="App">
       <div className="container">
-        
         <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <Navigation />
-            <Routes>
-              <Route exact path="/" element={<MainMenu />} />
-              <Route path="/draw/:mode" element={<DrawingBoard />} />
-              <Route path="/gallery" element={<Gallery />} />
-              <Route path="/drawing/:id" element={<DrawingDetails/>} />
-            </Routes>
-          </ThemeProvider>
+          <Navigation />
+          <Routes>
+            <Route exact path="/" element={<MainMenu />} />
+            <Route path="/draw/:mode" element={<DrawingBoard />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/drawing/:id" element={<DrawingDetails/>} />
+          </Routes>
         </BrowserRouter>
       </div>
     </div>
