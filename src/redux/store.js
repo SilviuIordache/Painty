5b5e967c07a8bbbc074485f69c0d4902c49136bc
Silviu Iordache <silviu.iordache92@gmail.com>
2022-03-31 21:49:19 +0300

    Remove unused slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from "./features/counterSlice";
 import toolReducer from './features/toolReducer';
-import canvasSlice from './features/canvasSlice';
-
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     tool: toolReducer,
-    canvas: canvasSlice
   },
 })
