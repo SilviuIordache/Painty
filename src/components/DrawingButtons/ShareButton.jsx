b82f4267d8c7daec560f2dc74b7e2791ae5814fc
Silviu Iordache <silviu.iordache92@gmail.com>
2022-04-22 17:21:22 +0300

    Change share button

--- src/components/DrawingButtons/ShareButton.jsx
+++ src/components/DrawingButtons/ShareButton.jsx
@@ -4,9 +4,9 @@ import copyBlobToClipboard from '../../helpers/copyBlobToClipboard';
 import dataURLtoBlob from '../../helpers/dataURLtoBlob';
 
 export default function ShareButton(props) {
-  async function shareImage(e) {
-    e.stopPropagation();
 
+  async function nativeShare(e) {
+    e.stopPropagation();
     if (navigator.share) {
       const blob = dataURLtoBlob(props.src);
       const imageFile = new File([blob], props.name, { type: blob.type });
@@ -24,21 +24,22 @@ export default function ShareButton(props) {
         text: shareText,
         file: [imageFile],
       });
-    } else {
-      // SPLIT THESE 2 INTO 2 BUTTONS: SHARE for mobile and COPY TO CLIPBOARD FOR OTHER
-      const blob = dataURLtoBlob(props.src);
-      await copyBlobToClipboard(blob);
-      setCopied(true);
     }
+  }
+
+  async function copyImageToClipboard(e) {
+    e.stopPropagation();
+    const blob = dataURLtoBlob(props.src);
+    await copyBlobToClipboard(blob);
+    setCopied(true);
   }
 
   const [copied, setCopied] = useState(false);
 
   return (
     <span>
-      <button className="btn btn-primary ms-1" onClick={shareImage}>
-        <span> SHARE </span>
-        <i className="fas fa-share"></i>
+      <button className="btn btn-primary ms-1" onClick={copyImageToClipboard}>
+        <i className="fas fa-copy"></i>
       </button>
 
       <Toast
