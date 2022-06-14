adc72ab973335d3d21dcb6fb89f09b0ea2746313
Silviu Iordache <silviu.iordache92@gmail.com>
2022-06-14 22:26:58 +0300

    Add icons to links

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import UserName from '../GalleryDrawing/UserName';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HomeIcon from '@mui/icons-material/Home';
+import CollectionsIcon from '@mui/icons-material/Collections';
+import ExploreIcon from '@mui/icons-material/Explore';
 
 export default function Navigation() {
 
@@ -8,14 +12,24 @@ export default function Navigation() {
   return (
     <div className="row mb-2 py-3 bg-light rounded d-flex justify-content-between">
       <div className="col-4">
-        <Link to="/" className="me-5">Home</Link>
+        <Link to="/" className="me-5 d-flex">
+          <HomeIcon/>
+          <span className="ms-1">Home</span>
+        </Link>
       </div>
-      <div className="col-3 d-flex justify-content-end">
+      <div className="col-4 d-flex justify-content-between">
         { currentUser ?
           <>
-            <Link to="/explore" className="me-2">Explore</Link>
-            <Link to="/gallery" className="me-2">Gallery</Link>
-            <Link to="/profile" className="me-2">
+            <Link to="/explore" className="me-2 d-flex">
+              <ExploreIcon/>
+              <span className="ms-1">Explore</span>
+            </Link>
+            <Link to="/gallery" className="me-2 d-flex">
+              <CollectionsIcon/>
+              <span className="ms-1">Gallery</span>
+            </Link>
+            <Link to="/profile" className="me-2 d-flex">
+              <AccountCircleIcon/>
               <UserName uid={currentUser.uid}/>
             </Link>
           </> :
