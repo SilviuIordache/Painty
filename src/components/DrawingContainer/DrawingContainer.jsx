0467665e20eac358c6a4a9292461ffea46541b76
Silviu Iordache <silviu.iordache92@gmail.com>
2022-07-17 19:59:22 +0300

    Improve loading

--- src/components/DrawingContainer/DrawingContainer.jsx
+++ src/components/DrawingContainer/DrawingContainer.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
 import { getImageFile } from '../../dbservices/images/getImageFile.js';
-import DrawingLoader from './DrawingLoader';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 export default function DrawingContainer(props) {
   const [imageSrc, setImageSrc] = useState();
@@ -19,19 +20,38 @@ export default function DrawingContainer(props) {
     };
   }, [props.path]);
 
+  const [drawingLoaderHeight, setDrawingLoaderHeight] = useState();
+  const drawingLoader = useRef();
+  useEffect(() => {
+    const width = drawingLoader.current.offsetWidth;
+
+    setDrawingLoaderHeight((width * 2) / 3);
+  }, []);
+
   return (
-    <>
-      {imageSrc ? (
+    <Box
+      ref={drawingLoader}
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: `${drawingLoaderHeight}px`,
+        backgroundColor: 'white',
+      }}
+    >
+      {!imageSrc ? (
+        <CircularProgress />
+      ) : (
         <img
           id={props.id}
           src={imageSrc}
           alt={props.name}
           key={props.index}
           width="100%"
+          height="100%"
         />
-      ) : (
-        <DrawingLoader />
       )}
-    </>
+    </Box>
   );
 }
